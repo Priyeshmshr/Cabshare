13daded3fd6a5377e7975c05424016b044e1db80
Deepak <gotodeepak@gmail.com>
2016-01-22 01:49:11 +0530

    dao part 1.5

--- dao/src/main/java/com/cabshare/server/dao/mysqldao/EntityDAO/UserDAO.java
+++ dao/src/main/java/com/cabshare/server/dao/mysqldao/EntityDAO/UserDAO.java
@@ -1,13 +1,13 @@
 package com.cabshare.server.dao.mysqldao.EntityDAO;
 
 import com.cabshare.server.dao.mysqldao.MySQLDao;
+import com.cabshare.server.entities.EntityPOJO.UserProfilePOJO;
 import com.cabshare.server.entities.Interfaces.Users.UserProfile;
-import com.sun.corba.se.spi.orbutil.fsm.Guard;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -24,14 +24,36 @@ public class UserDAO extends MySQLDao  {
         super(URL, username, password);
     }
 
-    public List<UserProfile> getUsersByID( String emailID)
-    {
-
+    public List<UserProfilePOJO> getUserBy(String key, String value) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("Select ?,?,?,?,? from User where ?=?");
+        preparedStatement.setString(1, com.cabshare.server.entities.schema.UserProfile.email);
+        preparedStatement.setString(2, com.cabshare.server.entities.schema.UserProfile.Date_Joined);
+        preparedStatement.setString(3, com.cabshare.server.entities.schema.UserProfile.gender);
+        preparedStatement.setString(4, com.cabshare.server.entities.schema.UserProfile.name);
+        preparedStatement.setString(5, com.cabshare.server.entities.schema.UserProfile.phone_number);
+        preparedStatement.setString(6, key);
+        preparedStatement.setString(7, value);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        ArrayList<UserProfilePOJO> userProfilePOJOs = new ArrayList<UserProfilePOJO>();
+        while (resultSet.next()) {
+            UserProfilePOJO userProfile = new UserProfilePOJO();
+            userProfile.setEmailID(resultSet.getString(com.cabshare.server.entities.schema.UserProfile.email));
+            userProfile.setDate(resultSet.getDate(com.cabshare.server.entities.schema.UserProfile.Date_Joined));
+            userProfile.setGender(resultSet.getString(com.cabshare.server.entities.schema.UserProfile.gender));
+            userProfile.setPhoneNumber(resultSet.getString(com.cabshare.server.entities.schema.UserProfile.phone_number));
+            userProfile.setName(resultSet.getString(com.cabshare.server.entities.schema.UserProfile.name));
+            userProfilePOJOs.add(userProfile);
+        }
+        return userProfilePOJOs;
     }
 
-    public void insertUserProfile( UserProfile userProfile)
-    {
-
+    public void insertUserProfile(UserProfile userProfile) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("Insert into User values  ?,?,?,?,? from User where ?=?");
+        preparedStatement.setString(1, com.cabshare.server.entities.schema.UserProfile.email);
+        preparedStatement.setString(2, com.cabshare.server.entities.schema.UserProfile.Date_Joined);
+        preparedStatement.setString(3, com.cabshare.server.entities.schema.UserProfile.gender);
+        preparedStatement.setString(4, com.cabshare.server.entities.schema.UserProfile.name);
+        preparedStatement.setString(5, com.cabshare.server.entities.schema.UserProfile.phone_number);
     }
 
     public void queryUserTable ( String query) throws SQLException {
---
+++ entities/src/main/java/com/cabshare/server/entities/schema/AndroidGCMregistration.java
@@ -0,0 +1,10 @@
+package com.cabshare.server.entities.schema;
+
+/**
+ * Created by deepak on 21/1/16.
+ */
+public class AndroidGCMregistration {
+
+    public final static String emailID = "emailID";
+    public final static String gcmRegistrationID = "GCMregistrationID";
+}
